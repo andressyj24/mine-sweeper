b08584e3d41115c682e54ff80e038e28ae706191
Andres Salazar <andressyj24@gmail.com>
2021-05-08 22:33:15 -0500

    Added libraries to support Swagger documentation and a GameController to test Swagger

--- src/main/java/com/exercise/minesweeper/adapters/SpringFoxConfig.java
+++ src/main/java/com/exercise/minesweeper/adapters/SpringFoxConfig.java
@@ -1,4 +1,4 @@
-package com.exercise.minesweeper;
+package com.exercise.minesweeper.adapters;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
