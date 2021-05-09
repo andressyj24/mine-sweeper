fd1c5b4677a7ddf4ab850f4714ef889beafbc6f2
Andres Salazar <andressyj24@gmail.com>
2021-05-08 23:09:54 -0500

    Fixed class name to match

--- src/main/java/com/exercise/minesweeper/adapters/RestGameController.java
+++ src/main/java/com/exercise/minesweeper/adapters/RestGameController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class RestTestController {
+public class RestGameController {
 
     @GetMapping("/")
     public String greeting() {
